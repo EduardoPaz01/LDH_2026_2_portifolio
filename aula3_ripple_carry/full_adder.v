db2c28828df1106d8f59907ed3f0f4af5cbfa46f
EduardoPaz01 <eduardopazputti@gmail.com>
2026-09-01 11:31:31 -0300

    [misc] Updated full adder

--- aula3_ripple_carry/full_adder.v
+++ aula3_ripple_carry/full_adder.v
@@ -1,3 +1,4 @@
+`timescale 1us/1us 
 
 module full_adder (
 	input a, // wire está implícito no input a, ou seja, "input wire a"
@@ -8,7 +9,21 @@ module full_adder (
 );
 
 	// Modelagem dataflow
-	assign s = ( a & b & ci ) | ( ~a & b & ~ci ) | ( ~a & ~b & ci ) | ( a & ~b & ~ci );
-	assign co = ( a & b ) | ( ci & b ) | ( ci & a );
+	//assign s = ( a & b & ci ) | ( ~a & b & ~ci ) | ( ~a & ~b & ci ) | ( a & ~b & ~ci );
+	//assign co = ( a & b ) | ( ci & b ) | ( ci & a );
+	
+	// Modelagem Carry out
+	wire w1, w2, w3;
+	and #1 u1 (w1, a,  b);
+	and #1 u2 (w2, ci, b);
+	and #1 u3 (w3, ci, a);
+	or  #1 u4 (co , w1, w2, w3);
+	
+	wire w4, w5, w6, w7;
+	and #1 u5 (w4, a, b, ci);
+	and #1 u6 (w5, ~a, b, ~ci);
+	and #1 u7 (w6, ~a, ~b, ci);
+	and #1 u8 (w7, a, ~b, ~ci);
+	or  #1 u9 (s , w4, w5, w6, w7);
 	
 endmodule
