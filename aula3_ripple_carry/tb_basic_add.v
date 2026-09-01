8caf55403db20d1f2aede79b8cdd204d4683990b
EduardoPaz01 <eduardopazputti@gmail.com>
2026-09-01 11:08:55 -0300

    [misc] Updated testbunk

--- aula3_ripple_carry/tb_basic_add.v
+++ aula3_ripple_carry/tb_basic_add.v
@@ -15,13 +15,54 @@ module tb_basic_add;
 	// Estímulos
 	initial begin // Bloco que executa apenas uma vez, Esse bloco não é sintetizado, usado apenas para debug e tb
 		a = 0; b = 0;
+		#1
+		$monitor("Test [1]: %b + %b = %b ", a, b, s);
+		#1
+		if(s==5'd0) begin
+			$monitor("Test [1] : PASS");
+		end else begin
+			$monitor("Test [1] : FAIL");
+		end
+		
 		#10
+		
+
 		a = 1; b = 1;
+		#1
+		$monitor("Test [2]: %b + %b = %b ", a, b, s);
+		#1
+		if(s==5'd2) begin
+			$monitor("Test [2] : PASS");
+		end else begin
+			$monitor("Test [2] : FAIL");
+		end
+		
 		#10
+		
 		a = 3; b = 5;
+		#1
+		$monitor("Test [3]: %b + %b = %b ", a, b, s);
+		#1
+		if(s==5'd8) begin
+			$monitor("Test [3] : PASS");
+		end else begin
+			$monitor("Test [3] : FAIL");
+		end
+			
 		#10
+		
 		a = 7; b = 9;
+		#1
+		$monitor("Test [4]: %b + %b = %b ", a, b, s);
+		#1
+		if(s==5'd16) begin
+			$monitor("Test [4] : PASS");
+		end else begin
+			$monitor("Test [4] : FAIL");
+		end
+		
 		#10
+		
 		$finish;
 	end 
 	
