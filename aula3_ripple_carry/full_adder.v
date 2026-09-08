9b9109f83c699ad3ec03aefd2419ec57b5a1dd4d
EduardoPaz01 <eduardopazputti@gmail.com>
2026-09-08 09:27:48 -0300

    [misc] Added structural modeling
    Utilização das primitivas em modelagem estrutural ao invés da dataflow

--- aula3_ripple_carry/full_adder.v
+++ aula3_ripple_carry/full_adder.v
@@ -1,4 +1,4 @@
-`timescale 1us/1us 
+`timescale 1ns/1ns
 
 module full_adder (
 	input a, // wire está implícito no input a, ou seja, "input wire a"
@@ -12,18 +12,24 @@ module full_adder (
 	//assign s = ( a & b & ci ) | ( ~a & b & ~ci ) | ( ~a & ~b & ci ) | ( a & ~b & ~ci );
 	//assign co = ( a & b ) | ( ci & b ) | ( ci & a );
 	
-	// Modelagem Carry out
+	// Modelagem Estrutural
+	// 	: Saída carry out
 	wire w1, w2, w3;
-	and #1 u1 (w1, a,  b);
-	and #1 u2 (w2, ci, b);
-	and #1 u3 (w3, ci, a);
-	or  #1 u4 (co , w1, w2, w3);
+	and #20 u1 (w1, a,  b);// não podemos ligar saídas em um reg, somente wire, pelo fato de serem conexões
+	and #20 u2 (w2, ci, b);
+	and #20 u3 (w3, ci, a);
+	or  #30 u4 (co , w1, w2, w3);
 	
+	// 	: Saída soma
+	wire na, nb, nci;
 	wire w4, w5, w6, w7;
-	and #1 u5 (w4, a, b, ci);
-	and #1 u6 (w5, ~a, b, ~ci);
-	and #1 u7 (w6, ~a, ~b, ci);
-	and #1 u8 (w7, a, ~b, ~ci);
-	or  #1 u9 (s , w4, w5, w6, w7);
+	not #10 u10 (na, a);
+	not #10 u11 (nb, b);
+	not #10 u12 (nci, c);
+	and #30 u5 (w4, a, b, ci);
+	and #30 u6 (w5, na, b, nci);
+	and #30 u7 (w6, na, nb, ci);
+	and #30 u8 (w7, a, nb, nci);
+	or  #40 u9 (s , w4, w5, w6, w7);
 	
 endmodule
